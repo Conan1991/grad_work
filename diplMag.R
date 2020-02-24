490a3534b220d6b272694d6c8afb5cba7e9fc19e
conan1991 <spellbuilder1991@gmail.com>
2020-02-24 15:21:31 +0300

    new changes

--- diplMag.R
+++ diplMag.R
@@ -169,7 +169,7 @@ y = x
 
 step_x = 0.00045
 step_y = 0.00045
-step_t = (step_x^2/lambda)/1000
+step_t = (step_x^2/lambda)/100
 
 shiftU = shiftV = matrix(data = 0, nrow = rows, ncol = cols)
 colnames(shiftU)= colnames(shiftV) = x
@@ -202,7 +202,7 @@ checkResidual = function(U, it)
     max_residuals <-c(max_residuals,max(residuals))
     
   }
-  #print(max_residuals)
+  print(max_residuals)
   for(i in 1:length(max_residuals))
   {
     maximum = max_residuals[i]
@@ -240,22 +240,25 @@ for (t in 2:number_of_iterations) {
   }
   SHIFTS_U[[t]] = shiftU
   SHIFTS_V[[t]] = shiftV
+  ##fit first layers
+ for(k in 2:(cols-1))
+ {
+   SHIFTS_U[[t]][1,k]=lambda*step_x/(2*step_y)*(SHIFTS_V[[t-1]][2,k+1]-SHIFTS_V[[t-1]][2,k-1])/(lambda+2*nu) - 3*lambda*alpha*step_x*temperature[2,1]/(lambda+2*nu) + SHIFTS_U[[t]][2,k]
+   SHIFTS_V[[t]][1,k] = step_x/(2*step_y)*(SHIFTS_U[[t-1]][2,k+1]-SHIFTS_U[[t-1]][2,k-1])+SHIFTS_V[[t]][2,k]
+   SHIFTS_U[[t]][rows, k]= SHIFTS_U[[t]][rows-1, k] - lambda*step_x/(2*step_y)*(SHIFTS_V[[t-1]][rows-1, k+1]-SHIFTS_V[[t-1]][rows-1,k-1])/(lambda+2*nu)+3*lambda*alpha*step_x*temperature[1,rows]/(lambda+2*nu)
+   SHIFTS_V[[t]][rows , k]= SHIFTS_V[[t]][rows-1, k] - step_x/(2*step_y)*(SHIFTS_U[[t-1]][rows-1,k+1]-SHIFTS_U[[t-1]][rows-1,k-1])
+ }
+  #cat("Operation=", t ,"\n")
+  #print(SHIFTS_U[[t]])
 }
 
 #fit first and last layers
-for (t in 2:number_of_iterations)
-  for(j in 2:(cols-1))
-  {
-    SHIFTS_U[[t]][1,j]=lambda*step_x/(2*step_y)*(SHIFTS_V[[t]][2,j+1]-SHIFTS_V[[t]][2,j-1])/(lambda+2*nu) - 3*lambda*alpha*step_x*temperature[2,1]/(lambda+2*nu) + SHIFTS_U[[t]][2,j]
-    SHIFTS_V[[t]][1,j] = step_x/(2*step_y)*(SHIFTS_U[[t]][2,j+1]-SHIFTS_U[[t]][2,j-1])+SHIFTS_V[[t]][2,j]
-    SHIFTS_U[[t]][rows, j]= SHIFTS_U[[t]][rows-1, j] - lambda*step_x/(2*step_y)*(SHIFTS_V[[t]][rows-1, j+1]-SHIFTS_V[[t]][rows-1,j-1])/(lambda+2*nu)+3*lambda*alpha*step_x*temperature[1,rows]/(lambda+2*nu)
-    SHIFTS_V[[t]][rows , j]= SHIFTS_V[[t]][rows-1, j] - step_x/(2*step_y)*(SHIFTS_U[[t]][rows-1,j+1]-SHIFTS_U[[t]][rows-1,j-1])
-#print(SHIFTS_U[[t]][i,1])
-  }
+#for (t in 2:number_of_iterations)
+  
 
 #print(checkResidual(SHIFTS_U, number_of_iterations - 500))
 #print(checkResidual(SHIFTS_U, number_of_iterations - 500))
 
-plot(SHIFTS_U[[500]][2,], xaxt="n")
+plot(SHIFTS_U[[100]][2,], xaxt="n")
 axis(1, at = c(1:(cols)), labels = x)
-lines(SHIFTS_U[[500]][2,],col="red")
+lines(SHIFTS_U[[100]][2,],col="red")
